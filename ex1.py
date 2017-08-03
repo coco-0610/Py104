39631ced0af06b9f0e99e170c9fe454e26cdafd9
COCO <cocoxuke0610@foxmail.com>
2017-08-03 21:55:15 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,25 @@
+# encoding: utf-8  
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'." 
+print 'I "said" do not touch this.'
+
+# 附加题
+	# 1
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'." 
+print 'I "said" do not touch this.'
+print 'this is my add line'
+	# 2
+print 'print only one line '
+	# 3 注释
+
+
+
