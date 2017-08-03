4aea718b516bdea1fabcecac5130f1dd3602a5f7
COCO <cocoxuke0610@foxmail.com>
2017-08-03 22:10:36 +0800

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# encoding: utf-8  
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ") 
+weight = raw_input("How much do you weigh? ")
+print "So, you're %r old, %r tall and %r heavy." % ( age, height, weight)
+
+# pydoc是python自带的一个文档生成工具，使用pydoc可以很方便的查看类和方法结构
