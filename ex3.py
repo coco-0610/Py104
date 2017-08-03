0b9fd2518b73140fc2f1c7c3126025d4f64dbdf6
COCO <cocoxuke0610@foxmail.com>
2017-08-03 21:56:02 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,31 @@
+# print code
+print "I will now count my chickens:"
+# this is plus and slash
+print "Hens", 25 + 30 / 6
+# this is minus and asterisk and percent
+print "Roosters", 100 - 25 * 3 % 4
+# this is text
+print "Now I will count the eggs:"
+# hah
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# this boolean
+print "Isittruethat3+2<5-7?"
+# this is boolean
+print 3+2<5-7
+# this is text and 5
+print "Whatis3+2?",3+2 
+#Whatis5-7? -2
+print "Whatis5-7?",5-7
+# Oh, that's why it's False.
+print "Oh, that's why it's False."
+# How about some more.
+print "How about some more."
+# Is it greater? True
+print "Is it greater?", 5 > -2
+# Is it greater or equal? True
+print "Is it greater or equal?", 5 >= -2 
+# Is it less or equal? False
+print "Is it less or equal?", 5 <= -2
+
+# ###### fujiati
+print 5+3/2
