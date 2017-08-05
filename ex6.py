3f6c22cd8110431e5a409b52a325832934d508a3
COCO <cocoxuke0610@foxmail.com>
2017-08-05 11:52:18 +0800

    Create ex6.py

---
+++ ex6.py
@@ -0,0 +1,23 @@
+# define x 
+x = "There are %d type of people." % 10
+# define binary and do_not
+binary = "binary"
+do_not = "don't"
+# define y  
+y = "Those who know %s and those who %s." % (binary , do_not)
+# export x and y  
+print x
+print y
+print "I said: %r." % x
+print "I also said:'%s'." % y 
+# define the value of hailrious 
+hailrious = False
+# define joke_evaluation
+joke_evaluation = "Isn't that joke so funny?! %r"
+# export joke_evaluation
+print joke_evaluation % hailrious
+# define w and e 
+w="This is the left said of ..."
+e="a string with a right side."
+# export w plus e 
+print w+e
